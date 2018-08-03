f486e02335108706fb3b0ad4c005e78a5fb09675
mig1415 <41913862+mig1415@users.noreply.github.com>
2018-08-03 14:27:37 +0400

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,12 +1,46 @@
 <?
-$character_mask = " \t\n\r\0\x0B";
-$name=trim($_POST['name'],$character_mask);
-$surname=trim($_POST['surname'],$character_mask);
-$age=trim($_POST['age'],$character_mask);
+//header('Content-type: text/html; charset=UTF-8');
 //////////////////////////////////////////////////////
 $db = mysqli_connect("37.140.192.112","u0128123_vks","grisha123009","u0128123_vks") or die ("ошибка"); 
 $utf8="set names utf8";
 $result = mysqli_query($db,$utf8);
-$set = "INSERT INTO NutnetTest(name,surname,age) VALUES ('$name','$surname','$age')"; 
+$set = "SELECT * FROM NutnetTest WHERE age >18 ORDER BY surname ASC";
 $result = mysqli_query($db,$set);
+///////////////////////////////////////////////////
+while($row=mysqli_fetch_array($result))
+{
+$add = 'nameG='.urlencode($row['name']).'&surnameG='.urlencode($row['surname']).'&ageG='.urlencode($row['age']);//решение проблемы кодировки
+$i=$i+1;
+$d[$i]=$add;
+}
 mysqli_close($db);
+//////////////////////////////////////////////////////
+$j=1;
+while ($d[$j])
+{
+echo 'создана запись '.$j.'</br>';
+$url = 'https://script.google.com/macros/s/AKfycbzOICsSVgT2EfElFZU8O3Ddc-_K9Dxe3SNOkJ43spK3SBnksc4/exec?';
+$dataurl=$d[$j];
+echo $url.$dataurl;
+$k = curl_init();
+curl_setopt($k, CURLOPT_HEADER, 0);
+curl_setopt($k, CURLOPT_URL, $url.$dataurl);
+curl_exec($k);
+curl_close($k);
+$j=$j+1;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
